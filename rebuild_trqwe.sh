6984bc87a3ce30fe2ef0caaa76b25857eb11b4a5
traversc <traversc@gmail.com>
2017-07-12 15:34:47 -1000

    init

--- rebuild_trqwe.sh
+++ rebuild_trqwe.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-cd ~/Desktop/trqwe_git
+cd ~/Desktop/trqwe
 Rscript -e "library(Rcpp); compileAttributes('trqwe');"
 Rscript -e "library(roxygen2); roxygenise('trqwe');"
 
 
 #RRO=/usr/lib64/RRO-3.2.2/R-3.2.2/bin/R
-
-R CMD build trqwe
+R CMD build .
 R CMD INSTALL trqwe_0.1.tar.gz
+R CMD Rd2pdf trqwe -o "manual.pdf" --no-preview --force
 
-R CMD Rd2pdf trqwe -o "manual.pdf"
-#$RRO CMD INSTALL trqwe_0.1.tar.gz
+git add .
+git commit -am 'init'
+git push trqwe master
