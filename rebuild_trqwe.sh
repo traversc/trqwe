adc5510a04d9bc34d70c04719014fe7b480df595
Travers Ching <tching@adaptivebiotech.com>
2018-06-19 14:08:47 -0700

    init

--- rebuild_trqwe.sh
+++ rebuild_trqwe.sh
@@ -6,7 +6,7 @@ Rscript -e "library(roxygen2); roxygenise('.');"
 #RRO=/usr/lib64/RRO-3.2.2/R-3.2.2/bin/R
 R CMD build .
 R CMD INSTALL trqwe_0.1.tar.gz
-R CMD Rd2pdf . -o "manual.pdf" --no-preview --force
+# R CMD Rd2pdf . -o "manual.pdf" --no-preview --force
 
 git add .
 git commit -am 'init'
